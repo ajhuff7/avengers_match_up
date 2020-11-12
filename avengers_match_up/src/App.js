be79891f7c56c56ccab567ff0e53d9ef759db602
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-11-11 21:24:07 -0800

    randomizer added

--- avengers_match_up/src/App.js
+++ avengers_match_up/src/App.js
@@ -4,33 +4,91 @@ import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import avengers from "./avengers.json";
 
+
+function randomize(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let x = Math.floor(Math.random() * (i+1));
+    let hold = array[i];
+    array[i] = array[x];
+    array[x] = hold
+  }
+  return array
+};
+
+
 class App extends Component {
   // Setting this.state.avengers to the friends json array
   state = {
-    avengers: avengers
+    avengers, 
+    score: 0,
+    highScore: 0,
+    chooseCard: []
   };
+
 
   chooseAvenger = id => {
     // Filter this.state.avengers for avengers with an id not equal to the id being removed
-    const avengers = this.state.avengers.filter(avenger => avenger.id !== id);
-    // Set this.state.friends equal to the new friends array
-    this.setState({ avengers: avengers });
-  };
-
-  // Map over this.state.friends and render a FriendCard component for each friend object
-  render() {
-    return (
-      <Wrapper>
-        <Title>Avengers Assemble</Title>
-        {this.state.avengers.map(avenger => (
-          <AvengerCard
-            id={avenger.id}
-            image={avenger.image}
-          />
-        ))}
-      </Wrapper>
-    );
+      let chooseCard = this.state.chooseCard;
+      let score = this.state.score;
+      let highScore = this.state.highScore;
+  
+      if (chooseCard.indexOf(id) === -1) {
+        chooseCard.push(id);
+        this.scoreIncrement();
+        this.randoAvenger();
+      } 
+      
+      else if (this.state.score === 12) {
+        alert("WINNER")
+        this.setState({
+          score: 0,
+          chooseCard: []
+        });
+      } 
+      
+      else {
+        this.setState({
+          score: 0,
+          chooseCard: []
+        });
+        alert("YOU LOSE")
+      }
+  
+      if (score > highScore) {
+        highScore = score;
+        this.setState({ highScore })
+      }
+    };
+  
+    scoreIncrement = () => {
+      this.setState({ score: this.state.score + 1 });
+    }
+  
+    randoAvenger = () => {
+      this.setState({ avengers: randomize(avengers) })
+    }
+  
+  
+    render() {
+      return (    
+        <Wrapper>
+           <Title 
+              score={this.state.score}
+              highScore={this.state.highScore} />
+  
+              {this.state.avengers.map(avenger => (
+                <AvengerCard
+                  id={avenger.id}
+                  image={avenger.image}
+                  chooseAvenger={this.chooseAvenger}
+                  />
+              ))}
+        
+      
+        </Wrapper>
+      ); 
+    }
   }
-}
+  
+  export default App;
 
-export default App;
