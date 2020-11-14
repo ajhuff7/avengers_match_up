593225af5e78ae2096dcf0890ed25cba5aaa1a3c
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-11-14 01:24:18 -0800

    corrected

--- avengers_match_up/src/components/Title/index.js
+++ avengers_match_up/src/components/Title/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title"> THE REAL QUEENS GAMBIT {props.children} Score: {props.score} High Score: {props.highScore}</h1>;
+  return <h1 className="title"> AVENGERS ASSEMBLE {props.children} Score: {props.score} High Score: {props.highScore}</h1>;
 }
 
 export default Title;
