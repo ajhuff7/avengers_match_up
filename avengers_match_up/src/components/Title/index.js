0a96def091b091f4e6c0f40b7f8804cfa0eb8066
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-12-29 12:13:02 -0800

    spacing

--- avengers_match_up/src/components/Title/index.js
+++ avengers_match_up/src/components/Title/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 function Title(props) {
-  return <h1 className="title"> AVENGERS ASSEMBLE {props.children} Score: {props.score} High Score: {props.highScore}</h1>;
+  return <h1 className="title"> AVENGERS ASSEMBLE {props.children} &nbsp;&nbsp; Score: {props.score} &nbsp;&nbsp; High Score: {props.highScore}</h1>;
 }
 
 export default Title;
